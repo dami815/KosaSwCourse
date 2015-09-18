85b22ad2537adbb48de3f04d0d934fa99c9d76a7
dami815 <dmkim815@gmail.com>
2015-09-18 09:34:52 +0900

    Service Test

---
+++ SpringProgramming/src/test/java/com/mycompany/myapp/service/BoardServiceTest.java
@@ -0,0 +1,23 @@
+package com.mycompany.myapp.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mycompany.myapp.ApplicationContextLoader;
+import com.mycompany.myapp.dto.Board;
+
+public class BoardServiceTest extends ApplicationContextLoader {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@Test
+	public void testAdd() {
+		Board board = new Board();
+		board.setTitle("테스트1");
+		board.setContent("테스트 내용1");
+		board.setWriter("글쓴이1");
+		boardService.add(board);
+	}
+	
+}
